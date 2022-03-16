aaf4064b6ae5f215ac3c9a1a35d4a67f685ea304
jacob <jacoberichardson7@gmail.com>
2022-03-16 10:40:54 -0600

    discover page fully styled

--- backend/db/seeders/20220315214348-bird.js
+++ backend/db/seeders/20220315214348-bird.js
@@ -8,8 +8,8 @@ module.exports = {
         scientific_name: 'Bubo virginianus',
         family: 'Owls',
         description: 'Large and widespread owl with distinctive ear tufts. Found in a variety of habitats from dense woods to prairie and deserts with at least some trees. Also, found in wooded towns and suburbs. Typically well-camouflaged dark brown overall, but varies in color. Often engages in haunting duets, with males and females hooting back and forth. Preys upon a variety of animals, including mammals, birds and reptiles',
-        sounds: 'github string here!',
-        image: 'github string here!',
+        sounds: 'https://res.cloudinary.com/dd9pletih/video/upload/v1647389984/great_horned_owl_vnu5oc.mp3',
+        image: 'https://res.cloudinary.com/dd9pletih/image/upload/v1647389985/great_horned_owl_hnzqj2.jpg',
         range: 'Found all throughout the Americas, though most densely populated in North America',
         conservation_status: 'Least Concern'
       },
@@ -18,8 +18,8 @@ module.exports = {
         scientific_name: 'Panterpe insignis',
         family: 'Hummingbirds',
         description: 'If you see the throat in good light, there is no mistaking this beauty: no other hummingbird combines every color of the rainbow on the throat and breast. In poor light, could be mistaken for other fairly large hummingbirds like Talamanca Hummingbird. Look for dark blue-black tail, medium-length bill with pink at the base, and blue rump, or just wait for it to turn its head. Sexes alike. Found in montane forests and edges, mostly from 1500m to treeline. Frequently visits feeders',
-        sounds: 'github string here!',
-        image: 'github string here!',
+        sounds: 'https://res.cloudinary.com/dd9pletih/video/upload/v1647389983/fiery_throated_hummingbird_g2csua.mp3',
+        image: 'https://res.cloudinary.com/dd9pletih/image/upload/v1647389985/fiery_throated_hummingbird_ty9kvh.jpg',
         range: 'Found in Costa Rica and on the wester edge of Panama',
         conservation_status: 'Least Concern'
       }
--- frontend/src/components/Discover/index.js
+++ frontend/src/components/Discover/index.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import './Discover.css';
+import { NavLink } from 'react-router-dom';
 
 function Discover() {
   const allBirds = useSelector(state => state.birds.allBirds);
 
   if (!allBirds) {
     return (
-      <div>Loading...</div>
+      <div className='discover-loading'>Loading...</div>
     )
   }
 
   return (
     <div>
-      {/* <img src='https://user-images.githubusercontent.com/88405940/158489382-2fdfe4c4-67e4-429a-89e3-84e27b1680b0.jpeg' /> */}
-      {/* <audio controls src='https://res.cloudinary.com/dd9pletih/video/upload/v1647389983/fiery_throated_hummingbird_g2csua.mp3' /> */}
       {
         allBirds.map(ele => (
-          <>
-            <div>
-              hiii
+          <div className='discover-main-div'>
+            <div className='discover-image-div'>
+              <NavLink to={`/discover/${ele.id}`}><img className='discover-image' src={ele.image} alt='bird-pic' /></NavLink>
             </div>
-          </>
+            <div className='discover-main-content'>
+              <div>
+                <NavLink to={`/discover/${ele.id}`} className='discover-name-link'><div className='discover-name'>{ele.common_name}</div></NavLink>
+                <div className='discover-sname'>{`- ${ele.scientific_name}`}</div>
+              </div>
+              <div>
+                <div className='discover-description-header'>Description:</div>
+                <div className='discover-description'>{ele.description}</div>
+              </div>
+              <audio className='discover-sound' controls src={ele.sounds} />
+            </div>
+          </div>
         ))
       }
     </div>
