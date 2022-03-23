46d2ca11841e7537bdfe3f9dbffc9c0f66ea83bf
jacob <jacoberichardson7@gmail.com>
2022-03-23 15:43:32 -0600

    changed github link to birder rather than to my profile

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -9,7 +9,7 @@ function About() {
       <img className='jake-image' src={jakeimage} alt='pic of me' />
       <div className='about-me'>I love to code, play music, eat, sleep, workout, hike, and birdwatch! I feel especially passionate about educating people on how to raise their quality of life and boldly pursue their dreams.</div>
       <div className='about-links'>
-        <a className='github' href='https://github.com/JakeRich7' target="_blank" rel="noreferrer"><FaGithub size={70} color='white' /></a>
+        <a className='github' href='https://github.com/JakeRich7/birder' target="_blank" rel="noreferrer"><FaGithub size={70} color='white' /></a>
         <a className='linkedin' href='https://www.linkedin.com/in/jacob-richardson-73440518/' target="_blank" rel="noreferrer"><FaLinkedin size={70} color='blue' /></a>
       </div>
     </div>
